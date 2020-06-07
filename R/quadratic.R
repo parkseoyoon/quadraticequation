1c9bf4e88ed308a7aa6ed5a2136bf1ffe07b30df
seoyoonpark <s21810698@stat01.yu.ac.kr>
2020-06-07 22:38:33 +0900

    solve quadratic equation

---
+++ R/quadratic.R
@@ -0,0 +1,18 @@
+#' solve equation
+#'
+#' This function solves quadratic equation(ax^2 + bx^2 + c = 0)
+#'
+#' @examples
+#'
+#' quadratic(1, 2, -1)
+quadratic <- function(a, b, c){
+  d <- b^2 - 4*a*c
+  if (d < 0)
+    cat("Not real roots", "\n")
+  if (d >= 0){
+    x1 <- -b + sqrt(d)
+    x2 <- -b - sqrt(d)
+    cat("x1 = ", x1, "\n")
+    cat("x2 = ", x2, "\n")
+  }
+}
